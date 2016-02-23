2ba229c2affa9df7a933b68fe14c8e274b521775
Daniel Torok <sam@rarity.hu>
2016-02-24 00:30:56 +0100

    [TypeSystem] syntax parser loop

--- TypeSystem/1 - Typeless Lambda expressions/src/hu/elte/inf/toduabi/SyntaxParser.java
+++ TypeSystem/1 - Typeless Lambda expressions/src/hu/elte/inf/toduabi/SyntaxParser.java
@@ -1,12 +1,46 @@
 package hu.elte.inf.toduabi;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class SyntaxParser {
+	
+	private final String EXT_NAME = "_";
 
 	public SyntaxTree parse(ArrayList<LexParserItem> tokens) throws SyntaxParserException {
-		// TODO Auto-generated method stub
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < tokens.size(); i++) {
+			String name = tokens.get(i).getName();
+			char token = tokens.get(i).getToken();
+			
+			String nextName = EXT_NAME;
+			if (i+1 < tokens.size()) {
+				nextName = tokens.get(i+1).getName();
+			}
+			
+			stack.push(token);
+			if (name.equals(SharedConstants.OPEN) || name.equals(SharedConstants.LAMBDA) || name.equals(SharedConstants.DOT)) {
+				continue;
+			} else if (name.equals(SharedConstants.CLOSE)) {
+				checkAndReduce(stack);
+			} else if (name.equals(SharedConstants.VARIABLE)) {
+				checkAndReduce(stack, nextName);
+			}
+		}
+		if (!stack.isEmpty()) {
+			throw new SyntaxParserException("eof");
+		}
 		return null;
+	}
+
+	private void checkAndReduce(Stack<Character> stack, String nextName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void checkAndReduce(Stack<Character> stack) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
