c03df707ae26f21cb188a776e890613f7ebdd914
Daniel Torok <daniel.torok@sap.com>
2016-02-24 09:54:41 +0100

    [TypeSystem] syntax parser - checkAndReduce: variable, abstraction

--- TypeSystem/1 - Typeless Lambda expressions/src/hu/elte/inf/toduabi/SharedConstants.java
+++ TypeSystem/1 - Typeless Lambda expressions/src/hu/elte/inf/toduabi/SharedConstants.java
@@ -4,6 +4,8 @@ public class SharedConstants {
 	public static final String LAMBDA = "Lambda";
 	public static final String VARIABLE = "Variable";
 	public static final String APPLICATION = "Application";
+	public static final String ABSTRACTION = "Abstraction";
+	public static final String EXPRESSION = "Expression";
 	public static final String DOT = "Dot";
 	public static final String OPEN = "Open";
 	public static final String CLOSE = "Close";
--- TypeSystem/1 - Typeless Lambda expressions/src/hu/elte/inf/toduabi/SyntaxParser.java
+++ TypeSystem/1 - Typeless Lambda expressions/src/hu/elte/inf/toduabi/SyntaxParser.java
@@ -7,14 +7,19 @@ public class SyntaxParser {
 	
 	private final String EXT_NAME = "_";
 	private Stack<LambdaExpression> expressions;
+	
+	private final String VARIABLE_RULE = SharedConstants.VARIABLE;
+	private final String ABSTRACTION_RULE = SharedConstants.LAMBDA + SharedConstants.VARIABLE + SharedConstants.DOT + SharedConstants.EXPRESSION;
+	private final String APPLICATION_RULE = SharedConstants.EXPRESSION + SharedConstants.APPLICATION + SharedConstants.EXPRESSION;
+	private final String PARENTHESIS_RULE = SharedConstants.OPEN + SharedConstants.EXPRESSION + SharedConstants.CLOSE;
 
 	public SyntaxTree parse(ArrayList<LexParserItem> tokens) throws SyntaxParserException {
 		this.expressions = new Stack<LambdaExpression>();
-		Stack<Character> stack = new Stack<Character>();
+		Stack<LexParserItem> stack = new Stack<LexParserItem>();
 		
 		for (int i = 0; i < tokens.size(); i++) {
+			LexParserItem token = tokens.get(i);
 			String name = tokens.get(i).getName();
-			char token = tokens.get(i).getToken();
 			
 			String nextName = EXT_NAME;
 			if (i+1 < tokens.size()) {
@@ -34,9 +39,61 @@ public class SyntaxParser {
 		return null;
 	}
 
-	private void checkAndReduce(Stack<Character> stack, String nextName) {
-		// TODO Auto-generated method stub
+	private void checkAndReduce(Stack<LexParserItem> stack, String nextName) throws SyntaxParserException {
+		if (stack.isEmpty()) {
+			return;
+		}
 		
+		Stack<LexParserItem> prefix = new Stack<LexParserItem>();
+		String prefixString = "";
+		boolean reduced = false;
+		
+		while (!reduced || stack.size() != 0) {
+			LexParserItem item = stack.pop();
+			prefix.push(item);
+			prefixString = prefixString + item.getName();
+			
+			if (prefix.equals(VARIABLE_RULE)) {
+				
+				reduced = true;
+				expressions.push(new LambdaVariable(item.getToken()));
+				if (nextName.equals(SharedConstants.DOT)) {
+					stack.push(item);
+				} else {
+					stack.push(new LexParserItem('E', SharedConstants.EXPRESSION));
+					checkAndReduce(stack, nextName);
+				}
+				
+			} else if (prefix.equals(ABSTRACTION_RULE)) {
+				
+				reduced = true;
+				stack.push(new LexParserItem('E', SharedConstants.EXPRESSION));
+				
+				LambdaExpression expression = expressions.pop();
+				LambdaExpression _variable = expressions.pop();
+				if (!_variable.getClass().equals(LambdaVariable.class)) {
+					throw new SyntaxParserException("invalid abstraction rule found");
+				}
+				LambdaVariable variable = (LambdaVariable) _variable;
+				expressions.push(new LambdaAbstraction(variable.getVariable(), expression));
+				
+				checkAndReduce(stack, nextName);
+				
+			} else if (prefix.equals(APPLICATION_RULE)) {
+				
+			} else if (prefix.equals(PARENTHESIS_RULE)) {
+				
+			} else {
+				 continue;
+			}
+		}
+		
+		if (!reduced) {
+			while (!prefix.isEmpty()) {
+				stack.push(prefix.pop());
+			}
+		}
 	}
+	
 
 }
