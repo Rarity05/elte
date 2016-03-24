73d1af09eba8f5064656c3805dcb0ffa1855d588
Daniel Torok <daniel.torok@sap.com>
2016-03-24 14:10:57 +0100

    [Robotics] [OpenCV] added 'ratio' to ImageData
    Change-Id: I306f802d300d03a0b5d5347a5250fc7a2fbd731c

--- Robotics/library/robotsystem/src/main/java/hu/elte/inf/robotics/opencv/ImageData.java
+++ Robotics/library/robotsystem/src/main/java/hu/elte/inf/robotics/opencv/ImageData.java
@@ -5,20 +5,23 @@ import java.util.ArrayList;
  * 
  * This class represents the output of the OpenCV module.
  * It contains the Robot as a single property (RPoint), and the Boxes and Targets as a list of Points.
+ * Also contains the unit / centimeters ratio based on the Robot's hard-coded dimensions.
  * 
  */
 public class ImageData {
 	private ArrayList<Point> boxes;
 	private ArrayList<Point> targets;
 	private RPoint robot;
+	private int ratio;
 	
-	public ImageData(RPoint robot, ArrayList<Point> boxes, ArrayList<Point> targets) {
+	public ImageData(RPoint robot, ArrayList<Point> boxes, ArrayList<Point> targets, int ratio) {
 		if (boxes == null || targets == null) {
 			throw new IllegalArgumentException("Boxes and Targets can't be null.");
 		}
 		this.robot = robot;
 		this.boxes = boxes;
 		this.targets = targets;
+		this.ratio = ratio;
 	}
 	
 	/**
@@ -43,5 +46,13 @@ public class ImageData {
 	 */
 	public ArrayList<Point> getTargets() {
 		return this.targets;
+	}
+	
+	/**
+	 * 
+	 * @return integer the unit/centimeters ratio based on the hard-coded Robot's dimensions. 
+	 */
+	public int getRatio() {
+		return this.ratio;
 	}
 }
