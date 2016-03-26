12c90aa6796c29f257304b816375d9890d2d3a27
Daniel Torok <sam@rarity.hu>
2016-03-26 12:35:50 +0100

    [Robotics] [Controller] moved CommandType inside Command

--- Robotics/library/robotsystem/src/main/java/hu/elte/inf/robotics/controller/Command.java
+++ Robotics/library/robotsystem/src/main/java/hu/elte/inf/robotics/controller/Command.java
@@ -5,19 +5,33 @@ package hu.elte.inf.robotics.controller;
  *
  */
 public class Command {
-	private CommandType type;
+	private Type type;
 	private int value;
 	
-	public Command(CommandType type, int value) {
+	public Command(Type type, int value) {
 		this.type = type;
 		this.value = value;
 	}
 	
-	public CommandType getType() {
+	public Type getType() {
 		return this.type;
 	}
 	
 	public int getValue() {
 		return this.value;
+	}
+	
+	/**
+	 * 
+	 * Enumeration type for possible control commands.
+	 * TURN - indicates the Robot has to execute a turn maneuver.
+	 * FORWARD - the Robot has to accelerate.
+	 * BACKWRAEDS - the Robot has to accelerate backwards.
+	 * STOP - the Robot has to stop.
+	 * COLLISION - indicates that a collision event must be allowed.
+	 *
+	 */
+	public static enum Type {
+		TURN, FORWARD, BACKWARDS, STOP, COLLISION
 	}
 }
