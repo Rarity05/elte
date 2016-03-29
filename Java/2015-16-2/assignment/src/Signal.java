e30c5eec45c0dd36c42f093bb42fe4b1ad917e76
Daniel Torok <daniel.torok@sap.com>
2016-03-29 10:41:53 +0200

    [Jsva] [Assignment] Signal class  - listeners  - setValue  - subscribe
    Change-Id: I319f3f7e3997ffa6f2f4ee49147e5163b797f271

--- Java/2015-16-2/assignment/src/Signal.java
+++ Java/2015-16-2/assignment/src/Signal.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
 
 public class Signal<T> {
 	private T value;
-
+	private ArrayList<ISignalAction<T>> actions;
+	
+	public Signal() {
+		this.actions = new ArrayList<ISignalAction<T>>();
+	}
+	
 	public T getValue() {
 		return this.value;
+	}
+	
+	/**
+	 * Sets a new value to the Signal
+	 * Iterates through the subscribed listeners,
+	 * notifying everybody who is interested in this signal
+	 * @param value the new value
+	 */
+	public void setValue(T value) {
+		T oldValue = this.value;
+		this.value = value;
+		
+		for (ISignalAction<T> action : actions) {
+			try {
+				action.onSignalChanged(oldValue, value);
+			} catch (Exception e) {
+				System.out.println("[ERROR]: There was an error in one of the ISignalActions");
+			}
+		}
+	}
+	
+	public void subscribe(ISignalAction<T> action) {
+		if (action != null) {
+			this.actions.add(action);
+		}
 	}
 }
