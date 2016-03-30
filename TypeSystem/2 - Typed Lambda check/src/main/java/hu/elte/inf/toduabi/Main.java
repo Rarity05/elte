b1201477e1d5320e41199526ab067978b1c7fb77
Daniel Torok <daniel.torok@sap.com>
2016-03-30 09:12:34 +0200

    [TypeSystem] [2] main class - console reader
    Change-Id: I57d565f92c0dbadf116584993f7bcf8105c0199e

---
+++ TypeSystem/2 - Typed Lambda check/src/main/java/hu/elte/inf/toduabi/Main.java
@@ -0,0 +1,20 @@
+package hu.elte.inf.toduabi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+	    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	    String input;
+	    while ((input = in.readLine()) != null && input.length() != 0) {
+		    try {
+		    } catch (Exception e) {
+		    }
+	    }
+	}
+
+}
