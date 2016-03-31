81d352039dbc5abd412250fef78133c3ae9a506a
Daniel Torok <daniel.torok@sap.com>
2016-03-31 13:09:30 +0200

    [TypeSystem] [2] SyntaxCallback documentation
    Change-Id: I8bc1d8fa2446414e3c15ea6d87b6b474d1825917

--- TypeSystem/2 - Typed Lambda check/src/main/java/hu/elte/inf/toduabi/ISyntaxCallback.java
+++ TypeSystem/2 - Typed Lambda check/src/main/java/hu/elte/inf/toduabi/ISyntaxCallback.java
@@ -5,6 +5,15 @@ import java.util.Stack;
 
 public interface ISyntaxCallback<T, K, R> {
 
+	/**
+	 * 
+	 * @param rule the String rule which was recognized by the SyntaxParser
+	 * @param prefixList the sub-stack in list format, for which the recognized rule applies
+	 * @param stack the whole sub-stack
+	 * @param nextType the next lexical element's type (outside the sub-stack)
+	 * @param expressions container for instantiated types (must contain a single element after when the nextType is null)
+	 * @return
+	 */
 	<T extends ILexicalItem<K>> SyntaxParser.ReturnType foundRule(String rule, ArrayList<T> prefixList, Stack<T> stack, K nextType, Stack<R> expressions);
 
 }
