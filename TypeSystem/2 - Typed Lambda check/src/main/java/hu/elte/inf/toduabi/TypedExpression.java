eb6ec0dbeaf5a56e3d7b77df350b90b66a1cf42a
Daniel Torok <daniel.torok@sap.com>
2016-04-05 13:09:44 +0200

    [TypeSystem] [2] TypedExpression class
    Change-Id: I0eb3817ee50e554d4e0eef42bb9ba407691ab94b

---
+++ TypeSystem/2 - Typed Lambda check/src/main/java/hu/elte/inf/toduabi/TypedExpression.java
@@ -0,0 +1,25 @@
+package hu.elte.inf.toduabi;
+
+public class TypedExpression {
+	private TypeContext typeContext;
+	private ILambdaExpression expression;
+	private IType type;
+	
+	public TypedExpression(TypeContext typeContext, ILambdaExpression expression, IType type) {
+		this.typeContext = typeContext;
+		this.expression = expression;
+		this.type = type;
+	}
+	
+	public TypeContext getTypeContext() {
+		return this.typeContext;
+	}
+	
+	public ILambdaExpression getExpression() {
+		return this.expression;
+	}
+	
+	public IType getType() {
+		return this.type;
+	}
+}
