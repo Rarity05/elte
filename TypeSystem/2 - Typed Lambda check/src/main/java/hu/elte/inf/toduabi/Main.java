e108567e0221df5e66e69302cde3a3553eb3746c
Daniel Torok <daniel.torok@sap.com>
2016-04-05 13:52:08 +0200

    [TypeSystem] [2] Main class
    Change-Id: I342fabed892ba8924606dbabe0402fdd309aec90

--- TypeSystem/2 - Typed Lambda check/src/main/java/hu/elte/inf/toduabi/Main.java
+++ TypeSystem/2 - Typed Lambda check/src/main/java/hu/elte/inf/toduabi/Main.java
@@ -12,7 +12,12 @@ public class Main {
 	    String input;
 	    while ((input = in.readLine()) != null && input.length() != 0) {
 		    try {
-		    } catch (Exception e) {
+		    	TypedExpression expression = TypeCheck.parseAndCheck(input);
+		    	System.out.println("OK: " + expression.toString());
+		    } catch (LexParserException | SyntaxParserException e) {
+		    	System.out.println("ERROR: " + input);
+		    } catch (TypeCheckException e) {
+		    	System.out.println("ERROR: " + e.getMessage());
 		    }
 	    }
 	}
