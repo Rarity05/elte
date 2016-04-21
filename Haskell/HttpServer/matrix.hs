060003bf860acfa6b57929628be04b6ba5fc590a
Daniel Torok <daniel.torok@sap.com>
2016-04-21 12:44:22 +0200

    [Haskell] [Matrix] outlines
    Change-Id: Ibc20333a2fdd6e4dac85aa317bd9e3668981fb54

--- Haskell/HttpServer/matrix.hs
+++ Haskell/HttpServer/matrix.hs
@@ -3,3 +3,63 @@ data Matrix = M
   , mSize :: Int
   , mIx :: (Int, Int) -> Maybe Int
   }
+
+--newMatrix :: Int -> Matrix
+--setMatrix :: (Int, Int) -> Integer -> Matrix -> Matrix
+--getMatrix :: (Int, Int) -> Matrix -> Maybe Integer
+--addMatrix :: Matrix -> Matrix -> Maybe Matrix
+--mulMatrix :: Matrix -> Matrix -> Maybe Matrix
+
+-- TESTS
+
+{-
+test_newMatrix :: [Bool]
+test_newMatrix =
+  let m3 = newMatrix 3 in
+  [ mDat m3 == Seq.replicate 9 0
+  , mSize m3 == 3
+  , mIx m3 (0, 0) == Nothing
+  , mIx m3 (1, 1) == Just 0
+  , mIx m3 (2, 3) == Just 5
+  , mIx m3 (3, 2) == Just 7
+  ]
+-}
+{-
+test_setMatrix :: [Bool]
+test_setMatrix =
+  let m2 = newMatrix 2 in
+  [ mDat (setMatrix (0, 0) 1 m2) == mDat m2
+  , mDat (setMatrix (1, 1) 1 m2) == Seq.fromList [1, 0, 0, 0]
+  , mDat (setMatrix (1, 2) 1 m2) == Seq.fromList [0, 1, 0, 0]
+  ]
+-}
+{-
+test_getMatrix :: [Bool]
+test_getMatrix =
+  let m2 = setMatrix (1, 2) 1 $ newMatrix 2 in
+  [ getMatrix (0, 0) m2 == Nothing
+  , getMatrix (1, 1) m2 == Just 0
+  , getMatrix (1, 2) m2 == Just 1
+  ]
+-}
+{-
+test_addMatrix :: [Bool]
+test_addMatrix =
+  let ma = newMatrix 3
+      mb = setMatrix (1,2) 1 $ setMatrix (1,1) 1 $ newMatrix 2
+      mc = setMatrix (2,1) 1 $ setMatrix (1,1) 1 $ newMatrix 2
+  in  [ isNothing $ addMatrix ma mb
+      , (mDat <$> addMatrix mb mc) == Just (Seq.fromList [2, 1, 1, 0])
+      ]
+-}
+{-
+test_mulMatrix :: [Bool]
+test_mulMatrix =
+  let ma = newMatrix 3
+      mb = setMatrix (2,2) 2 $ setMatrix (1,1) 1 $ newMatrix 2
+      mc = setMatrix (2,1) 4 $ setMatrix (2,2) 3 $ setMatrix (1,1) 1 $ newMatrix 2
+  in  [ isNothing $ mulMatrix ma mb
+      , (mDat <$> mulMatrix mb mc) == Just (Seq.fromList [1, 0, 8, 6])
+      , (mDat <$> mulMatrix mc mb) == Just (Seq.fromList [1, 0, 4, 6])
+      ]
+-}
