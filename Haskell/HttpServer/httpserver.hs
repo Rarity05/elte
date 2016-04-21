29e1a6bdc0759f27f12bbd977ae22bdc57b93eb8
Daniel Torok <daniel.torok@sap.com>
2016-04-21 11:04:34 +0200

    [Haskell] [HttpServer] required code snippets
    Change-Id: I505c73416efb36b8e6bc4fbe49a8c7b58f01ecb0

---
+++ Haskell/HttpServer/httpserver.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+import Control.Concurrent
+import Control.Parallel.Strategies
+
+import Data.Text (Text)
+import qualified Data.Text     as Text
+import qualified Data.Text.IO  as Text
+
+import Data.Sequence (Seq, (|>))
+import qualified Data.Sequence as Seq
+
+import Data.Maybe
+import Data.String (fromString)
+import Network
+import System.IO
+
+import Test.QuickCheck
+import Test.QuickCheck.Test (isSuccess)
+
+hGetLines :: Handle -> IO [Text]
+hGetLines hdl = do
+  line <- Text.hGetLine hdl
+  if Text.null line
+  then return []
+  else do
+    rest <- hGetLines hdl
+    return $ line : rest
+
+acceptHandle :: Socket -> IO Handle
+acceptHandle serverSocket = do
+  (hdl, _, _) <- accept serverSocket
+  hSetBuffering hdl NoBuffering
+  hSetNewlineMode hdl $ NewlineMode CRLF CRLF
+  return hdl
+ 
+response :: Text -> Text
+response str = Text.unlines
+  [ "HTTP/1.0 200 OK"
+  , "Connection: close"
+  , "Content-Type: text/plain"
+  , ""
+  , str
+  ]
