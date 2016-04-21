61e0a6d4c830371784f4eb6d8b1dfca5af708972
Daniel Torok <daniel.torok@sap.com>
2016-04-21 17:03:37 +0200

    [Haskell] [QuickCheck] prop_drop2
    Change-Id: I63ed356c84473c2364ad12a758455400e4ab27cc

--- Haskell/HttpServer/quickcheck.hs
+++ Haskell/HttpServer/quickcheck.hs
@@ -19,6 +19,8 @@ left zl = zl
 prop_removeNegative :: [Int] -> Bool
 prop_removeNegative x = (foldl (*) 1 $ map (`div` 10) (removeNegative x)) >= 0
 
---prop_drop2 :: [a] -> Bool
+prop_drop2 :: [a] -> Bool
+prop_drop2 x = ((length x) - (length (drop2 x))) <= 2
+
 --prop_left :: ZippList a -> Bool
 --instance Arbitrary a => Arbitrary (ZippList a) where
