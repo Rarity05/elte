a14650a4785c7dc0331cde0f7a972993cd223ca2
Daniel Torok <daniel.torok@sap.com>
2016-04-21 11:07:18 +0200

    [Haskell] [HttpServer] tests
    Change-Id: I462ae90ba910482f68fd1bf359ea10d1813b09d9

--- Haskell/HttpServer/httpserver.hs
+++ Haskell/HttpServer/httpserver.hs
@@ -50,3 +50,56 @@ response str = Text.unlines
 --handleRequest :: Text -> (Int, Int) -> (Int, Int)
 --drawState :: (Int, Int) -> Text
 --main :: IO ()
+
+-- TESTS
+{-
+test_requestedResource =
+  [ requestedResource [""]                      == Nothing
+  , requestedResource ["ALMA"]                  == Nothing
+  , requestedResource ["GET"]                   == Nothing
+  , requestedResource ["GET /index.html"]       == Nothing
+  , requestedResource ["ALMA /index.html alma"] == Nothing
+  , requestedResource ["GET /index.html alma"]  == Just "index.html"
+  , requestedResource ["get /index.html alma"]  == Just "index.html"
+  , requestedResource ["GET /alma.html HTTP/1.1", "Korte: very"] == Just "alma.html"
+  ]
+-}
+{-
+test_handleRequest :: [Bool]
+test_handleRequest =
+  [ handleRequest "n" (2,3) == (2,2)
+  , handleRequest "s" (2,3) == (2,4)
+  , handleRequest "e" (2,3) == (3,3)
+  , handleRequest "w" (2,3) == (1,3)
+  , handleRequest "foobar" (2,3) == (2,3)
+  ]
+-}
+{-
+test_drawState :: [Bool]
+test_drawState =
+  [ drawState (-1, -1) ==
+    "..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \(-1,-1)"
+  , drawState (1, 5) ==
+    "..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \X.........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \..........\n\
+    \(1,5)"
+  ]
+-}
